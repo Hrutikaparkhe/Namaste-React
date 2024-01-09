451ad890f47a4bb34f8a6747f11cf0e9ec8b24a5
Hrutikaparkhe <you@example.comrutikaparkhe@gmail.com>
2024-01-09 12:00:05 -0800

    Added fixes for java setup

---
+++ DSA/JavaProject/src/App.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ 
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("get a");
+        String A=sc.next();
+        
+        System.out.println("Hello, World!"+ A);
+    }
+}
