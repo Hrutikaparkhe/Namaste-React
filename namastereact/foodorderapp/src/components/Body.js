a255eb00da93fba6844d7219bfcb90c1528e8807
Hrutikaparkhe <you@example.comrutikaparkhe@gmail.com>
2024-01-17 12:27:04 -0800

    Added real time API use instead of mockData feature ,shimmer UI, Toggle Login -Logout, Search Bar functionality in App (Episode 06)

--- namastereact/foodorderapp/src/components/Body.js
+++ namastereact/foodorderapp/src/components/Body.js
@@ -1,28 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./Card";
-import dataList from "./utils/mockData";
+import ShimmerUI from "./ShimmerUI";
 const Body = () => {
-  const [restList, setRestList] = useState(dataList);
-
-  return (
+  const [restList, setRestList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [searchText, setSearcheText] = useState("");
+  useEffect(() => {
+    fetchApiData();
+  }, []);
+  const fetchApiData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const filtereArr = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setRestList(filtereArr);
+    setFilteredList(filtereArr);
+  };
+  return restList.length > 0 ? (
     <div className="body">
       <div className="container">
-        <button
-          className="container-btn"
-          onClick={() => {
-            const filteredList = restList.filter((restData) => restData.info.avgRating > 4.5);
-            setRestList(filteredList);
-          }}
-        >
-          Top rated Restaurants
-        </button>
+        <div className="search-container">
+          <input
+            className="search-input"
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setSearcheText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const searchedData = restList.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+              setFilteredList(searchedData);
+            }}
+          >
+            Search
+          </button>
+          <button
+            className="container-btn"
+            onClick={() => {
+              const filteredList = restList.filter((restData) => restData.info.avgRating > 4);
+              setRestList(filteredList);
+              setFilteredList(filteredList);
+            }}
+          >
+            Top rated Restaurants
+          </button>
+        </div>
+
         <div className="card">
-          {restList.map((restaurant) => (
+          {filteredList.map((restaurant) => (
             <Card key={restaurant.info.id} restData={restaurant} />
           ))}
         </div>
       </div>
     </div>
+  ) : (
+    <ShimmerUI />
   );
 };
 export default Body;
