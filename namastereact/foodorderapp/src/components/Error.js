f5e78c591322089469bb1190e386f5fdf446a4de
Hrutikaparkhe <you@example.comrutikaparkhe@gmail.com>
2024-01-18 22:51:16 -0800

    added children routing , added abput, contact, menucard componet templates(EP 07)

---
+++ namastereact/foodorderapp/src/components/Error.js
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div className="error-conatiner">
+      <h1>Ooops...</h1>
+      <h3>Something went wrong!!!!</h3>
+    </div>
+  );
+};
+export default Error;
---
+++ namastereact/foodorderapp/src/components/RestaurantMenu.js
@@ -0,0 +1,4 @@
+const RestaurantMenu = () => {
+  return <div className="menu-container"></div>;
+};
+export default RestaurantMenu;
